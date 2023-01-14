66b2db11f882cd39d4d61bb0cb1ee029adb5a1d8
Berke Filiz <edu.berkefiliz@gmail.com>
2023-01-14 23:55:29 +0100

    Change lesson list into a grid

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Easy Web</title>
-        <link rel="stylesheet" href="styles/styles.css" />
-        <link rel="stylesheet" href="styles/homepage.css" />
-        <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-        />
-    </head>
-    <body>
-        <?php include "./components/header.php"; ?>
-        <div id="content">
-            <?php
-                if (isset($logged)) {
-                    echo "
+
+<head>
+    <title>Easy Web</title>
+    <link rel="stylesheet" href="styles/styles.css" />
+    <link rel="stylesheet" href="styles/homepage.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+</head>
+
+<body>
+    <?php include "./components/header.php"; ?>
+    <div id="content">
+        <?php
+        if (isset($logged)) {
+            echo "
                         <div id='welcome' class='info'>
                             <h3>Welcome!</h3>
                             <p>Currently logged in as user @$logged!</p>
                             <p>Soon here will be a progress bar!</p>
                         </div>
                     ";
-                } else {
-                    echo "
+        } else {
+            echo "
                         <div id='welcome' class='info'>
                             <h3>Web design, simplified</h3>
                             <p>Learn the very basics without difficult terminology.</p>
@@ -36,25 +35,29 @@
                             <button id='login' class='button-lg button-outline' onclick=\"window.location = '/login.php'\">Login</button>
                         </div>
                     ";
-                }
-            ?>
-        </div>
-    </body>
+        }
+        ?>
+    </div>
+</body>
+
 </html>
 
-<?php include "./serverfunctions/makeHomepage.php"; ?>
+<?php include "./serverfunctions/makeHomepage.php"; 
+?>
 
 <script>
     let content = document.getElementById("content");
     SECTIONS.forEach((section) => {
-        content.innerHTML += `
+        let newcontent = "";
+        newcontent += `
             <h2 id=section-${section.id}>${section.title}</h2>
+            <div class="item-grid">
         `;
         let lessons = LESSONS.filter(
             (lesson) => lesson.section == section.id
         ).sort((a, b) => (a.level > b.level ? 1 : -1));
         lessons.forEach((lesson) => {
-            content.innerHTML += `
+            newcontent += `
                 <div class="item">
                     <div class="img-wrapper">
                         <img src="./thumbnails/${lesson.image}.png" />
@@ -62,9 +65,17 @@
                     <div class="desc-wrapper">
                         <p class="name">${lesson.title}</p>
                         <p class="desc">${lesson.desc}</p>
+                        <div class="detail">
+                            <span><i class="fa-solid fa-eye"></i>0</span>
+                            <span><i class="fa-solid fa-comment"></i></i>0</span>
+                            <span><i class="fa-solid fa-star"></i></i>5.0</span>
+                        </div>
                     </div>
                 </div>
             `
         });
+        newcontent += "</div>";
+        console.log(newcontent)
+        content.innerHTML += newcontent;
     });
 </script>
