3231d70b29f4e173b55c7e56596180b01b842c09
Berke Filiz <edu.berkefiliz@gmail.com>
2023-01-14 22:51:01 +0100

    Remove searchbar

--- components/header.php
+++ components/header.php
@@ -7,6 +7,7 @@ include "./serverfunctions/loginCheck.php";
     header {
         background-color: #2d3032;
         border-radius: 1em;
+        padding: 2rem;
         box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px,
             rgba(0, 0, 0, 0.23) 0px 3px 6px;
     }
@@ -21,63 +22,31 @@ include "./serverfunctions/loginCheck.php";
     nav {
         margin: 0 auto;
         max-width: 76rem;
-        padding: 2rem;
-    }
-
-    nav {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         font-size: 1rem;
     }
 
-    #nav-buttons > a {
+    nav {
+        padding: 1.8rem 0 0 0;
+    }
+
+    .nav-buttons > a {
         text-decoration: none;
         color: #2d3032;
         transition: color 0.1s ease-in-out;
         display: block;
-        float: right;
+        float: left;
         margin-left: 1.5rem;
-        margin-top: 0.5rem;
     }
 
-    #nav-buttons > a > i {
+    .nav-buttons > a > i {
         margin-right: 0.5rem;
     }
 
-    #nav-buttons > a:hover {
+    .nav-buttons > a:hover {
         color: #2d97cb;
-    }
-
-    #search {
-        flex: 0.8;
-        max-width: 30rem;
-        display: flex;
-    }
-
-    #search > i {
-        padding: 0.5rem;
-    }
-
-    #search > input {
-        flex: 1;
-        border: none;
-        padding: 0.5rem;
-        box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px,
-            rgba(0, 0, 0, 0.23) 0px 3px 6px;
-    }
-
-    #search > input:focus {
-        outline: 1px solid #666;
-    }
-
-    #search > button {
-        border: none;
-        padding: 0.5rem;
-        box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px,
-            rgba(0, 0, 0, 0.23) 0px 3px 6px;
-        cursor: pointer;
-        margin-left: 0.5rem;
     }
 
     @media screen and (max-width: 80rem) {
@@ -94,17 +63,17 @@ include "./serverfunctions/loginCheck.php";
     <a style="margin: 0" href="/">Easy Web</a>
 </header>
 <nav>
-    <div id="search">
-        <i class="fa-solid fa-magnifying-glass"></i>
-        <input type="text" placeholder="Search" />
-        <button>Go</button>
+    <div class="nav-buttons">
+        <a href="/"><i class="fa-solid fa-house"></i> Home</a>
+        <a href="/"><i class="fa-solid fa-book"></i> Resources</a>
+        <a href="/"><i class="fa-solid fa-at"></i> Contact</a>
     </div>
-    <div id="nav-buttons">
+    <div class="nav-buttons">
+        <a href="https://github.com/berkefiliz/easyweb" target="_blank"
+            ><i class="fa-solid fa-code"></i> Source</a
+        >
         <a href="/serverfunctions/logoutUser.php"
             ><i class="fa-solid fa-arrow-right-from-bracket"></i> Logout</a
-        >
-        <a href="https://github.com/berkefiliz/easyweb" , target="_blank"
-            ><i class="fa-solid fa-code"></i> Source</a
         >
     </div>
 </nav>
