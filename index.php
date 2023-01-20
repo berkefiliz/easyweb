ef0f370f3e4e802a4ca863c9667409bb2dc10fd1
Berke Filiz <edu.berkefiliz@gmail.com>
2023-01-21 00:23:17 +0100

    Rename homepage.css to index.css for consistency

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <title>Easy Web</title>
     <link rel="stylesheet" href="styles/styles.css" />
-    <link rel="stylesheet" href="styles/homepage.css" />
+    <link rel="stylesheet" href="styles/index.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
 </head>
 
